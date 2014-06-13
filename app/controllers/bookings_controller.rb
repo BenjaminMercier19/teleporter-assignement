00c34b06c20ea776200480cbff3edcb38b0cae9f
Benjamin Mercier <benjamin.mercier19@gmail.com>
2014-06-13 13:08:18 -0300

    Finalise all models end db

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,12 +3,6 @@ class BookingsController < ApplicationController
 		@places = Place.all
   end
 
-	def show
-		debugger
-		@teleporter = Teleporter.find(params[:id])
-		@customers = Customer.joins(:teleporter).where("teleporters.id = :tel_id", {tel_id: params[:id]})
-	end
-
   def search
 		@teleporters = Teleporter.where("departure = :dep_id AND destination = :dest_id", {dep_id: params[:departure_id], dest_id: params[:destination_id]})
 
@@ -16,9 +10,40 @@ class BookingsController < ApplicationController
 			t.departure = Place.find(t.departure).name
 			t.destination = Place.find(t.destination).name
 		end
+		render 'search_without_result' if @teleporters.empty?
 	end
 
-	def new_booking
-		 	
+	def show
+		#debugger
+		@teleporter = Teleporter.find(params[:id])
+		@teleporter.departure = Place.find(@teleporter.departure).name
+		@teleporter.destination = Place.find(@teleporter.destination).name
+		@customers = Customer.joins(:teleporter).where("teleporters.id = :tel_id", {tel_id: params[:id]})
+		render 'show_without_booking' if @teleporter.full?
 	end
+	
+	def new_booking
+	 	@teleporter = Teleporter.find(params[:id])
+	end
+
+	def create
+		#debugger
+		teleporter = Teleporter.find(params[:id])
+		if params[:mail] != '' && params[:name] != ''
+			customer = Customer.where("mail = :mail", {mail: params[:mail]})
+			if !customer.empty?
+				flash[:error] = "#{customer.name} already booked a travel, sorry."
+				redirect_to new_booking_path(teleporter)
+			else
+				@customer = Customer.create!({:name => params[:name], :mail => params[:mail]})
+	 			@booking = Booking.create!({:customer_id => @customer.id, :teleporter_id => params[:id]})
+				flash[:notice] = "Congrats #{@customer.name}, your seat is book in the teleporter."
+				redirect_to teleporter_path(teleporter)
+			end
+		else
+			flash[:error] = "Please fill the form"
+			redirect_to new_booking_path(teleporter)
+		end
+  end
+
 end
